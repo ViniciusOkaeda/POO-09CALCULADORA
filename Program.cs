a3e25ffc2d647aa2a908daafa189d4aa1bcb11a2
Vinicius <viniciusokaeda@gmail.com>
2020-06-03 13:39:51 -0300

    estrutura inicial feita

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AULA09CALCULADORA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
